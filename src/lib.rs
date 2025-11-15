c439a21f5974c0c945a37259b4335ab9fc38b92d
Adam Ulichny <abrooksny@protonmail.com>
2025-11-14 23:23:28 -0600

    updated metadata for pypi

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+// Copyright (c) 2025 Adam Ulichny
+//
+// This source code is licensed under the MIT OR Apache-2.0 license
+// that can be found in the LICENSE-MIT or LICENSE-APACHE files
+// at the root of this source tree.
+
 use pyo3::prelude::*;
 
 pub mod dist;
