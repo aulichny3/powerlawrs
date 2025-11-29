08aed529fe34c8d1250157485ae5d758c08be02c
Adam Ulichny <abrooksny@protonmail.com>
2025-11-29 12:37:47 -0600

    fixed readthedocs wrong versions number

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'powerlawrs'
 copyright = '2025, Adam Ulichny'
 author = 'Adam Ulichny'
-release = '0.4.0'
+release = '0.0.12'
 
 # -- General configuration ---------------------------------------------------
 
