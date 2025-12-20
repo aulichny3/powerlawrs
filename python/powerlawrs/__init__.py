498c9f8ac0348aea87bf7dfe36d3421af0d667d0
Adam Ulichny <112702260+aulichny3@users.noreply.github.com>
2025-12-20 13:24:20 -0600

    update in docs and powerlaw 0.0.25

--- python/powerlawrs/__init__.py
+++ python/powerlawrs/__init__.py
@@ -1,10 +1,10 @@
+# Copyright (c) 2025 Adam Ulichny
+#
+# This source code is licensed under the MIT OR Apache-2.0 license
+# that can be found in the LICENSE-MIT or LICENSE-APACHE files
+# at the root of this source tree.
+
 """
-Copyright (c) 2025 Adam Ulichny
-
-This source code is licensed under the MIT OR Apache-2.0 license
-that can be found in the LICENSE-MIT or LICENSE-APACHE files
-at the root of this source tree.
-
 powerlawrs: A Python package for analyzing power-law distributions.
 """
 
